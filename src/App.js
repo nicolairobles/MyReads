f3a29994ebd1d1d61f42bc69e74c7ef0e399e759
Nicolai Robles <nicolai.robles@gmail.com>
2018-03-09 22:32:40 -0500

    starter commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+    books: [],
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -11,6 +12,13 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      console.log(books)
+      this.setState({books})
+    })
   }
 
   render() {
